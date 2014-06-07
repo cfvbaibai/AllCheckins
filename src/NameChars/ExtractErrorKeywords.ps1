871c68cd3a5d4515960c80d6e004d5ca50055fcd
Tony Nie <tonywhitewhite@hotmail.com>
2014-06-07 19:50:35 +0800

    Add script to extract error keywords

---
+++ src/NameChars/ExtractErrorKeywords.ps1
@@ -0,0 +1,13 @@
+﻿$scriptDir = Split-Path $myInvocation.MyCommand.Path
+$logPath = "$scriptDir\log.log"
+if (-not (Test-Path $logPath)) {
+    "Cannot find log file at {0}. Exit!" -f $logPath | Write-Host -ForegroundColor Red
+    exit 1
+}
+$logs = Get-Content $logPath -Encoding UTF8
+$resultFilePath = "$scriptDir\error.txt"
+if (Test-Path $resultFilePath) { Remove-Item $resultFilePath }
+$matchInfo = $logs | Select-String -AllMatches "\[keyword = (?<Keyword>[^\]]+)\]"
+foreach ($match in $matchInfo.Matches) {
+    $match.Groups["Keyword"].Value | Out-File $resultFilePath -Append 
+}
