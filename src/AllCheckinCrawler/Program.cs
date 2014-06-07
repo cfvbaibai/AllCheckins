9bc2dd6a24b5148686a4b414de336d9aaf8f3095
Tony Nie <tonywhitewhite@hotmail.com>
2014-06-07 20:25:27 +0800

    Refactor Program.cs. Extract methods from big Main method

--- src/AllCheckinCrawler/Program.cs
+++ src/AllCheckinCrawler/Program.cs
@@ -5,11 +5,18 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
-namespace AllCheckinCrawler
+namespace AllCheckin.CrawlerCli
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private class CrawlContext
+        {
+            public ISequence<string> Sequence { get; set; }
+            public QueryType QueryType { get; set; }
+            public int Pause { get; set; }
+        }
+
+        public static void Main(string[] args)
         {
             try
             {
@@ -23,91 +30,123 @@ namespace AllCheckinCrawler
                     throw new ArgumentException("Invalid number of arguments!");
                 }
 
-                ISequence <string> sequence = null;
-                QueryType queryType = QueryType.Unknown;
+                CrawlContext context = GetCrawlContext(sequenceType, start, pause);
+                Crawler crawler = CreateCrawler(context);
 
-                if (sequenceType.Equals("NN"))
-                {
-                    var innerSequence = new NaturalNumberSequence();
-                    innerSequence.Seek(long.Parse(start));
-                    sequence = new TextSequence<long>(innerSequence);
-                    queryType = QueryType.IdCardNumber;
-                }
-                else if (sequenceType.Equals("N"))
-                {
-                    sequence = new NameSequence();
-                    sequence.Seek(start);
-                    queryType = QueryType.Name;
-                }
-                else if (sequenceType.Equals("RICN"))
-                {
-                    sequence = new RandomIdCardNumberSequence();
-                    queryType = QueryType.IdCardNumber;
-                }
-                else if (sequenceType.Equals("RN"))
-                {
-                    sequence = new RandomNameSequence();
-                    queryType = QueryType.Name;
-                }
-                else
-                {
-                    throw new ArgumentException("Invalid sequence type: " + sequenceType);
-                }
-
-                var crawler = new Crawler(sequence);
-                crawler.PauseInterval = pause;
-                string recordFilePath = Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-                    "AllCheckinRandomNameRecord.txt");
-
-                IDictionary<string, bool> processedKeywords = new Dictionary<string, bool>();
-                if (File.Exists(recordFilePath))
-                {
-                    using (StreamReader reader = new StreamReader(recordFilePath))
-                    {
-                        string line = null;
-                        while ((line = reader.ReadLine()) != null)
-                        {
-                            processedKeywords[line] = true;
-                        }
-                    }
-                }
-                Console.WriteLine("{0} processed keywords found!", processedKeywords.Count);
-                crawler.AfterCrawl += (sender, e) =>
-                {
-                    var progress = e.Progress;
-                    if (e.Error == null)
-                    {
-                        File.AppendAllText(recordFilePath, progress.CurrentKeyword + Environment.NewLine);
-                    }
-                    if (e.Error != null)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                    }
-                    Console.WriteLine(
-                        "[{0}/{1} keyword={2}] {3}",
-                        progress.Current, progress.Total, progress.CurrentKeyword, progress.Message);
-                    Console.ResetColor();
-                };
-                crawler.BeforeCrawl += (sender, e) =>
-                {
-                    var keyword = e.CurrentKeyword;
-                    if (processedKeywords.ContainsKey(keyword))
-                    {
-                        e.Cancel = true;
-                    }
-                    else
-                    {
-                        processedKeywords[keyword] = true;
-                    }
-                };
-                crawler.Crawl(max, queryType);
+                crawler.Crawl(max, context.QueryType);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 Trace.TraceError(e.ToString());
             }
+        }
+
+        private static Crawler CreateCrawler(CrawlContext context)
+        {
+            var crawler = new Crawler(context.Sequence);
+            crawler.PauseInterval = context.Pause;
+
+            if (context.QueryType == QueryType.Name)
+            {
+                ExtraSetupForNameBasedCrawler(crawler);
+            }
+
+            crawler.AfterCrawl += (sender, e) =>
+            {
+                var progress = e.Progress;
+                if (e.Error != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.WriteLine(
+                    "[{0}/{1} keyword={2}] {3}",
+                    progress.Current, progress.Total, progress.CurrentKeyword, progress.Message);
+                Console.ResetColor();
+            };
+
+            return crawler;
+        }
+
+        private static void ExtraSetupForNameBasedCrawler(Crawler crawler)
+        {
+            string recordFilePath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "AllCheckinRandomNameRecord.txt");
+            IDictionary<string, bool> processedKeywords = new Dictionary<string, bool>();
+
+            if (File.Exists(recordFilePath))
+            {
+                using (StreamReader reader = new StreamReader(recordFilePath))
+                {
+                    string line = null;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        processedKeywords[line] = true;
+                    }
+                }
+            }
+            Console.WriteLine("{0} processed keywords found!", processedKeywords.Count);
+
+            crawler.BeforeCrawl += (sender, e) =>
+            {
+                var keyword = e.CurrentKeyword;
+                if (processedKeywords.ContainsKey(keyword))
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    processedKeywords[keyword] = true;
+                }
+            };
+
+            crawler.AfterCrawl += (sender, e) =>
+            {
+                if (e.Error == null)
+                {
+                    File.AppendAllText(recordFilePath, e.Progress.CurrentKeyword + Environment.NewLine);
+                }
+            };
+        }
+
+        private static CrawlContext GetCrawlContext(string sequenceType, string start, int pause)
+        {
+            ISequence<string> sequence;
+            QueryType queryType;
+            if (sequenceType.Equals("NN"))
+            {
+                var innerSequence = new NaturalNumberSequence();
+                innerSequence.Seek(long.Parse(start));
+                sequence = new TextSequence<long>(innerSequence);
+                queryType = QueryType.IdCardNumber;
+            }
+            else if (sequenceType.Equals("N"))
+            {
+                sequence = new NameSequence();
+                sequence.Seek(start);
+                queryType = QueryType.Name;
+            }
+            else if (sequenceType.Equals("RICN"))
+            {
+                sequence = new RandomIdCardNumberSequence();
+                queryType = QueryType.IdCardNumber;
+            }
+            else if (sequenceType.Equals("RN"))
+            {
+                sequence = new RandomNameSequence();
+                queryType = QueryType.Name;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid sequence type: " + sequenceType);
+            }
+            return new CrawlContext
+            {
+                Sequence = sequence,
+                QueryType = queryType,
+                Pause = pause,
+            };
         }
     }
 }
